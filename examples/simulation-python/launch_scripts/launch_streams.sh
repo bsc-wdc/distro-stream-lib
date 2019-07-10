bb51a322562cb508b5e325230f9c3f7a20f551cb
CristianRCV <cristianrcv@gmail.com>
2019-07-10 14:00:02 +0200

    Add simulation synthetic example

---
+++ examples/simulation-python/launch_scripts/launch_streams.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+  
+  # Define script constants
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+  # Define environment
+  module load TrunkCRC
+
+  # Define customizable application constraints
+  export CORES_SIMULATION=48
+  export CORES_PROCESS=1
+  export CORES_MERGE=12
+
+  # Define application arguments
+  app_exec="${SCRIPT_DIR}/../simulation_streams.py"
+  stream_base_dir="${SCRIPT_DIR}/exec/"
+  num_simulations=2
+  num_files=10
+  simulation_sleep_time=1000
+  simulation_sleep_random_range=1000
+  process_sleep_time=1000
+  process_sleep_random_range=500
+  merge_sleep_time=5000
+  merge_sleep_random_range=2000
+
+  rm -rf "${stream_base_dir}"
+  mkdir -p "${stream_base_dir}"
+
+  # Enqueue job
+  enqueue_compss \
+    --num_nodes=3 \
+    --worker_in_master_cpus=36 \
+    --exec_time=60 \
+    --qos=debug \
+    --jvm_workers_opts="-Dcompss.worker.removeWD=false" \
+    --worker_working_dir=gpfs \
+    --pythonpath="${SCRIPT_DIR}" \
+    --streaming=FILES \
+    -d \
+    -g \
+    -t \
+    --summary \
+    "${app_exec}" \
+    "${stream_base_dir}" \
+    "${num_simulations}" "${num_files}" \
+    "${simulation_sleep_time}" "${simulation_sleep_random_range}" \
+    "${process_sleep_time}" "${process_sleep_random_range}" \
+    "${merge_sleep_time}" "${merge_sleep_random_range}"
